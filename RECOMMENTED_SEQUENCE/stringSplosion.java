7d39642276e6784f0e28b938be95c763ffa0f7bd
TakumiHANAUE <tkmhana@gmail.com>
2019-02-03 17:10:02 +0900

    stringSplosion program

---
+++ RECOMMENTED_SEQUENCE/stringSplosion.java
@@ -0,0 +1,21 @@
+public class stringSplosion {
+    public static void main(String[] args)
+    {
+        var retstr = new String();
+        retstr = func_stringSplosion("Code");
+        System.out.println(retstr);
+    }
+    
+    public static String func_stringSplosion(String str) {
+        String outstr = new String();
+
+        for (int i = 1; i < str.length(); i++)
+        {
+            String substr = str.substring(0, i);
+            outstr = outstr.concat(substr);
+        }
+        outstr = outstr.concat(str);
+
+        return outstr;
+    }
+}
